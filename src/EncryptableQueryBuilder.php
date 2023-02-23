6c108d6df2c8284d0127a26c32711fa80bf1b62e
Yoan Ballesteros <y.ballesteros@presta-module.com>
2023-02-23 16:11:56 +0100

    - Handle `where` receiving an array instead of a string for the column parameter (which is the case when using `updateOrCreate` and similar)

--- src/EncryptableQueryBuilder.php
+++ src/EncryptableQueryBuilder.php
@@ -29,9 +29,24 @@ class EncryptableQueryBuilder extends Builder {
      */
     public function where($column, $operator = null, $value = null, $boolean = 'and')
     {
-        if (method_exists($this->model, 'encryptable') && $this->model->encryptable($column)) {
-            list($value, $operator) = $this->prepareValueAndOperator($value, $operator, func_num_args() === 2);
-            $value = $this->model->encryptAttribute($value);
+        if (method_exists($this->model, 'encryptable')) {
+            // Handle associative array, such as when we use updateOrCreate, in which case column contains the first arg
+            if (!is_array($column)) {
+                if ($this->model->encryptable($column)) {
+                    list($value, $operator) = $this->prepareValueAndOperator($value, $operator, func_num_args() === 2);
+                    $value = $this->model->encryptAttribute($value);
+                }
+            } else {
+                // Build a list of columns we have to encrypt for the where to work properly
+                $columnsToEncrypt = array_intersect(array_keys($column), $this->model->encryptable);
+                // Once we have that list, we can apply the same behavior than for the "classic" case above
+                foreach ($columnsToEncrypt as $columnName) {
+                    list($newValue, $operator) = $this->prepareValueAndOperator($column[$columnName], $operator, func_num_args() === 2);
+                    $newValue = $this->model->encryptAttribute($newValue);
+                    // Replace the original column value with the encrypted one
+                    $column[$columnName] = $newValue;
+                }
+            }
         }
 
         return parent::where($column, $operator, $value, $boolean);
